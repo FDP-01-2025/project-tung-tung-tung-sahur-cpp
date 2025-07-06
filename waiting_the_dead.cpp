637238a2053bf1c6e5035f7018d20b604b51f9c9
DavidAlCrCa <00131025@uca.edu.sv>
2025-07-06 10:08:50 -0600

    Update waiting_the_dead.cpp

--- waiting_the_dead.cpp
+++ waiting_the_dead.cpp
@@ -10,4 +10,35 @@ using namespace std;
 
 void text(int text)
 {
+  switch (text)
+{
+case 1:
+    cout<<R"(    
+
+                                                    .--------.
+                                                .'  .----.  '.
+                                                /   /      \   \         
+                                                |   |  0  0  |   |        
+                                                |   |   --   |   |        
+                                                |   |  ----  |   |        
+                                                \   \/   /
+                                                '.          .'
+                                                    '-..-'           
+
+                                                    |||||
+                                                    /|||||\  )" 
+            
+    <<endl;
+    cout<<"\n                                                 ---CEA te observa---"<<endl;
+    getch();
+
+    cout<<"\n                                    --Estas apunto de apostar tu vida ten cuidado--"<<endl;
+    getch();
+    
+    cout<<R"(
+
+    Cea es un ser de grado 2 salido de la dimension de la estatica.
+                    Se dedica a hacer apuestas ilegales en el mundo de los vivos, se dice que si le ganas
+                    en su propio juego este te otorgara lo que el mundo anhela...
+      }
 }
